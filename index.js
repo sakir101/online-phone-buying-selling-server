9d93ddf3a97e36c4b4c20e8b6bd21b5c3eaac0e5
sakir101 <sakirhossainf99@gmail.com>
2022-11-25 12:55:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ttvi8dx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    const productCollection = client.db('mobileHunterUser').collection('mobileProducts');
+    const categoryCollection = client.db('mobileHunterUser').collection('phoneCategory');
+
+    try {
+        app.get('/products', async (req, res) => {
+            console.log('p')
+            const query = {};
+            const cursor = productCollection.find();
+            const products = await cursor.toArray();
+            const productsFilter = products.filter(product => product.booked === 'none')
+            res.send(productsFilter);
+        });
+
+        app.get('/category', async (req,res)=>{
+            const query = {};
+            const cursor = categoryCollection.find();
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+
+        app.get('/categoryone/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const category = await categoryCollection.findOne(query);
+            res.send(category);
+          });
+
+          
+        app.get('/allproducts', async (req, res) => {
+            console.log('p')
+            const query = {};
+            const cursor = productCollection.find();
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+
+run().catch(console.log);
+
+app.get('/', async (req, res) => {
+    res.send('Mobile Hunter server is running');
+})
+
+app.listen(port, () => console.log(`Mobile Hunter running on ${port}`))
