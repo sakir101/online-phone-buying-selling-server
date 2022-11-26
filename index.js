36d7efd3f616d1a25670a78824017118ef3ff073
sakir101 <sakirhossainf99@gmail.com>
2022-11-27 00:18:49 +0600

    delete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 
@@ -21,6 +22,7 @@ async function run() {
     const categoryCollection = client.db('mobileHunterUser').collection('phoneCategory');
     const userCollection = client.db('mobileHunterUser').collection('phoneUser');
     const bookingCollection = client.db('mobileHunterUser').collection('bookphone');
+    const paymentsCollection = client.db('mobileHunterUser').collection('payPrice');
 
     try {
         app.get('/products', async (req, res) => {
@@ -53,10 +55,27 @@ async function run() {
             res.send(products);
         });
 
+        app.post('/create-payment-intent', async (req, res) => {
+            const bookingPhone = req.body;
+            const price = bookingPhone.productPrice;
+            const amount = price * 100;
+
+            const paymentIntent = await stripe.paymentIntents.create({
+                currency: 'usd',
+                amount: amount,
+                "payment_method_types": [
+                    "card"
+                ]
+            });
+            res.send({
+                clientSecret: paymentIntent.client_secret,
+            });
+        });
+
+
         app.post('/mobileusers', async (req, res) => {
             const user = req.body;
             const result = await userCollection.insertOne(user);
-            console.log(result)
             res.send(result);
         });
 
@@ -66,20 +85,70 @@ async function run() {
             res.send(result);
         });
 
+        app.post('/payments', async (req, res) =>{
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            const id = payment.bookingId
+            const filter = {_id: ObjectId(id)}
+            const updatedDoc = {
+                $set: {
+                    payment: 'paid',
+                    transactionId: payment.transactionId
+                }
+            }
+            const updatedResult = await productCollection.updateOne(filter, updatedDoc)
+            res.send(result);
+        })
+
 
         app.get('/orders', async (req, res) => {
             const cursor = bookingCollection.find({ buyerEmail: { $in: [req.query.email] } });
             const orders = await cursor.toArray();
-            console.log(orders)
             res.send(orders)
         });
 
         app.get('/payment/:id', async(req,res)=>{
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
-            const item = await productCollection.findOne(query);
+            const item = await bookingCollection.findOne(query);
             res.send(item);
-        })
+        });
+
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await userCollection.findOne(query);
+            console.log(user)
+            res.send({ isSeller: user?.role === 'seller' });
+        });
+
+        app.get('/alluser/:email', async(req,res)=>{
+            const email = req.params.email;
+            const query = { email }
+            const user = await userCollection.findOne(query);
+           res.send(user);
+        });
+
+        app.post('/addproduct', async(req,res)=>{
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
+        });
+
+        app.get('/allproducts/:email', async(req,res)=>{
+            const sellerEmail = req.params.email;
+            const query = { sellerEmail }
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+           res.send(products);
+        });
+
+        app.delete('/deleteproduct/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+          })
 
     }
     finally {
